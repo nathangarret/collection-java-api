620a4cecafd95e3beee9acdd69a73429ac2ae78e
Nathan V Garrett <ngarrett768@gmail.com>
2023-08-15 20:41:00 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for(int x = 1; x > 0; i++){
+            System.out.println("i");
+        }
+    }
+}
