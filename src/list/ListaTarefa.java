67710995b9477b426a7ba4adb286ac43350fb90e
Nathan V Garrett <ngarrett768@gmail.com>
2023-08-15 21:09:07 -0300

    Resolvendo o desafio ListaTarefa
    Signed-off-by: Nathan V Garrett <ngarrett768@gmail.com>

---
+++ src/list/ListaTarefa.java
@@ -0,0 +1,58 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    // Atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    // void -> não vou retornar nada
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefa(){
+        System.out.println(tarefaList);
+    }
+
+    // psvm => public static void main
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        // System.out.println("O número total de elementos na lista é: "+ listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        // System.out.println("O número total de elementos na lista é: "+ listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        // System.out.println("O número total de elementos na lista é: "+ listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        // System.out.println("O número total de elementos na lista é: "+ listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é: "+ listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefa(); // list.Tarefa@3d075dc0 -> adicionar o toString()
+
+    }
+}
---
+++ src/list/Tarefa.java
@@ -0,0 +1,21 @@
+package list;
+
+public class Tarefa {
+
+    // Atributo
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
